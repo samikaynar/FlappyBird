ad51d3cb010dfb856083d5081db5e487a48f69b6
Sami <samikaynr41@gmail.com>
2025-09-01 17:30:45 -0400

    the code has been changed for being more professional

--- flapybird.py
+++ flapybird.py
@@ -1,75 +1,89 @@
 import pygame
-import random
-
-def draw_pipe(xx,yy):
-    screen.blit(pipe_image,(xx,yy))
-
-
-def draw_bird(x,y):
-    screen.blit(bird_image,(x,y))
-
-def draw_bg():
-    screen.blit(bg_image,(0,0))
-
-def draw_ground():
-    screen.blit(ground_image, (0, screen.get_height() - ground_image.get_height()))
-
-def game():
-    x=100
-    y=150
-    xx=400
-    yy=random.randint(-80,-5)
-    speed=0
-    gravity=0.5
-    jump=-10
-    
-
-    running=True
-    clock=pygame.time.Clock()
-    while running:
-        
-        clock.tick(60)  # 60 frames per second
-        for event in pygame.event.get():
-            if event.type==pygame.QUIT:
-                running=False
-
-        key=pygame.key.get_pressed()
-        if key[pygame.K_SPACE]:
-            speed=jump
-
-        speed=speed+gravity
-        y=y+speed
-        xx=xx-5
-
-        
-        draw_bg()
-        draw_bird(x,y)
-        draw_pipe(xx,yy)
-        draw_ground()
-        pygame.display.update()
-    
-    
+from pygame.locals import *
 
 pygame.init()
-screen=pygame.display.set_mode((427,468))
 
-pygame.display.set_caption("Welcome to my flapy bird game!!")
-bird_image = pygame.image.load("C:\\Users\\kayna\\OneDrive\\Masaüstü\\python\\flapybird\\bird1.png")
-pipe_image=pygame.image.load("C:\\Users\\kayna\\OneDrive\\Masaüstü\\python\\flapybird\\pipe.png")
-bg_image=pygame.image.load("C:\\Users\\kayna\\OneDrive\\Masaüstü\\python\\flapybird\\bg.png")
-ground_image=pygame.image.load("C:\\Users\\kayna\\OneDrive\\Masaüstü\\python\\flapybird\\ground.png")
-bird_image = pygame.transform.scale(bird_image, (bird_image.get_width() // 2, bird_image.get_height() // 2))
-pipe_image = pygame.transform.scale(pipe_image, (pipe_image.get_width() // 2, pipe_image.get_height() // 2))
-bg_image = pygame.transform.scale(bg_image, (bg_image.get_width() // 2, bg_image.get_height() // 2))
-ground_image=pygame.transform.scale(ground_image, (ground_image.get_width() // 2, ground_image.get_height() // 2))
+clock = pygame.time.Clock()
+fps = 60
 
-game()
+screen_width = 432
+screen_height = 468
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Welcome to my Flappy Bird Gamee')
 
-    
-    
+#bird class
+class Bird(pygame.sprite.Sprite):
+	def __init__(self,x, y):
+		pygame.sprite.Sprite.__init__(self)
+		self.images=[]
+		self.index=0
+		self.counter=0
+		for num in range(1, 4):
+			img = pygame.image.load(f'C:\\Users\\kayna\\OneDrive\\Masaüstü\\python\\flapybird\\bird{num}.png')
+			img_width=img.get_width()
+			img_height=img.get_height()
+			img=pygame.transform.scale(img ,(img_width // 2 , img_height // 2) )
+			self.images.append(img)
+		self.image=self.images[self.index]
+		self.rect=self.image.get_rect()
+		self.rect.center= [x,y]
+
+	def update(self):
+		flappy_cooldown=5
+		self.counter+=1
+		if self.counter > flappy_cooldown:
+			self.index+=1
+			self.counter=0
+			if self.index >= len(self.images):
+				self.index=0
+
+		self.image=self.images[self.index]
 
 
 
 
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(40,int(screen_height / 2))
+
+bird_group.add(flappy)
+
+#define game variables
+ground_scroll = 0
+scroll_speed = 4
+
+#load images
+bg = pygame.image.load('C:\\Users\\kayna\\OneDrive\\Masaüstü\\python\\flapybird\\bg.png')
+ground_img = pygame.image.load('C:\\Users\\kayna\\OneDrive\\Masaüstü\\python\\flapybird\\ground.png')
+wight_bg=bg.get_width()
+height_bg=bg.get_height()
+wight_ground=ground_img.get_width()
+height_ground=ground_img.get_height()
+height_ground_real=height_ground / 2
+bg=pygame.transform.scale(bg,(wight_bg // 2 , height_bg // 2))
+ground_img=pygame.transform.scale(ground_img , (wight_ground // 2 , height_ground // 2))
+
+run = True
+while run:
+
+	clock.tick(fps)
+
+	#draw background
+	screen.blit(bg, (0,0))
+	bird_group.draw(screen)
+	bird_group.update()
+
+	#draw and scroll the ground
+	screen.blit(ground_img, (ground_scroll, screen_height-height_ground_real))
+	ground_scroll -= scroll_speed
+	if abs(ground_scroll) > 16:
+		ground_scroll = 0
+
+
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+
+	pygame.display.update()
 
 pygame.quit()
